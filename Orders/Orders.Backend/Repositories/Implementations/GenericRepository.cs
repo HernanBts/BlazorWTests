d5d1545943ece2665c5fc65254546b835eb356d2
Hernán Bs. <bts.hernan@gmail.com>
2024-03-13 17:27:15 -0300

    - Add Repository - Add Unit of work - Add category controller - change to generic implementation - All in Backedn

---
+++ Orders/Orders.Backend/Repositories/Implementations/GenericRepository.cs
@@ -0,0 +1,141 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Orders.Backend.Data;
+using Orders.Backend.Repositories.Interfaces;
+using Orders.Shared.Responses;
+
+namespace Orders.Backend.Repositories.Implementations
+{
+    public class GenericRepository<T> : IGenericRepository<T> where T : class
+    {
+        private readonly DataContext _context;
+        private readonly DbSet<T> _entity;
+
+        public GenericRepository(DataContext context)
+        {
+            _context = context;
+            _entity = context.Set<T>();
+        }
+        public async Task<ActionResponse<T>> AddAsync(T entity)
+        {
+            _context.Add(entity);
+            try
+            {
+                await _context.SaveChangesAsync();
+                return new ActionResponse<T>
+                {
+                    WasSuccess = true,
+                    Result = entity
+                };
+            }
+            catch (DbUpdateException)
+            {
+                return DbUpdateExceptionActionResponse();
+            }
+            catch (Exception exception)
+            {
+                return ExceptionActionResponse(exception);
+            }
+        }
+
+        public async Task<ActionResponse<T>> DeleteAsync(int id)
+        {
+            var row = await _entity.FindAsync(id);
+            if (row == null)
+            {
+                return new ActionResponse<T>
+                {
+                    WasSuccess = false,
+                    Message = "Registro no encontrado."
+                };
+            }
+
+            try
+            {
+                _entity.Remove(row);
+                await _context.SaveChangesAsync();
+                return new ActionResponse<T>
+                {
+                    WasSuccess = true
+                };
+            }
+            catch (Exception)
+            {
+                return new ActionResponse<T> 
+                { 
+                    WasSuccess = true, 
+                    Message = "No se puede borrar por que tiene registros relacionados." 
+                };
+            }
+        }
+
+        public async Task<ActionResponse<IEnumerable<T>>> GetAsync()
+        {
+            var list = await _entity.ToListAsync();
+
+            return new ActionResponse<IEnumerable<T>>
+            {
+                WasSuccess = true,
+                Result = list
+            };
+
+        }
+
+        public async Task<ActionResponse<T>> GetAsync(int id)
+        {
+            var row = await _entity.FindAsync(id);
+            if (row == null)
+            {
+                return new ActionResponse<T>
+                {
+                    WasSuccess = false,
+                    Message = "Registro no encontrado."
+                };
+            }
+            return new ActionResponse<T>
+            {
+                WasSuccess = true,
+                Result = row
+            };
+        }
+
+        public async Task<ActionResponse<T>> UpdateAsync(T entity)
+        {
+            _context.Update(entity);
+            try
+            {
+                await _context.SaveChangesAsync();
+                return new ActionResponse<T>
+                {
+                    WasSuccess = true,
+                    Result = entity
+                };
+            }
+            catch (DbUpdateException)
+            {
+                return DbUpdateExceptionActionResponse();
+            }
+            catch (Exception exception)
+            {
+                return ExceptionActionResponse(exception);
+            }
+        }
+
+        private ActionResponse<T> ExceptionActionResponse(Exception exception)
+        {
+            return new ActionResponse<T>
+            {
+                WasSuccess = false,
+                Message = exception.Message
+            };
+        }
+
+        private ActionResponse<T> DbUpdateExceptionActionResponse()
+        {
+            return new ActionResponse<T>
+            {
+                WasSuccess = false,
+                Message = "Ya existe el registro que estas intentando crear."
+            };
+        }
+    }
+}
