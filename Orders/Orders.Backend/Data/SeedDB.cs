f97fbeccc19f42465ae40df3fa20fad55ea39108
Hernán Bs. <bts.hernan@gmail.com>
2024-03-25 18:46:44 -0300

    - Add seedDB

---
+++ Orders/Orders.Backend/Data/SeedDB.cs
@@ -0,0 +1,48 @@
+﻿
+using Orders.Shared.Entities;
+
+namespace Orders.Backend.Data
+{
+    public class SeedDB
+    {
+        private readonly DataContext _context;
+
+        public SeedDB(DataContext context)
+        {
+            _context = context;
+        }
+
+        public async Task SeedDbAsync()
+        {
+            await _context.Database.EnsureCreatedAsync();
+            await CheckCountriesAsync();
+            await CheckCategoriesAsync();
+        }
+
+        private async Task CheckCategoriesAsync()
+        {
+            if(!_context.Categories.Any()) 
+            {
+                _context.Categories.Add(new Category { Name = "Holgar" });
+                _context.Categories.Add(new Category { Name = "Tecnologia" });
+                _context.Categories.Add(new Category { Name = "Moda" });
+                _context.Categories.Add(new Category { Name = "Ropa" });
+
+                await _context.SaveChangesAsync();
+            }
+        }
+
+        private async Task CheckCountriesAsync()
+        {
+            if (!_context.Countries.Any())
+            {
+                _context.Countries.Add(new Country { Name = "Argentina" });
+                _context.Countries.Add(new Country { Name = "Chile" });
+                _context.Countries.Add(new Country { Name = "Colombia" });
+                _context.Countries.Add(new Country { Name = "EEUU" });
+
+                await _context.SaveChangesAsync();
+            }
+        }
+    }
+}
--- Orders/Orders.Backend/Program.cs
+++ Orders/Orders.Backend/Program.cs
@@ -13,10 +13,25 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddDbContext<DataContext>(x => x.UseSqlServer("name=LocalConnection"));
+builder.Services.AddTransient<SeedDB>();
+
 builder.Services.AddScoped(typeof(IGenericUnitOfWork<>), typeof(GenericUnitOfWork<>));
 builder.Services.AddScoped(typeof(IGenericRepository<>), typeof(GenericRepository<>));
 
 var app = builder.Build();
+
+SeedData(app);
+
+void SeedData(WebApplication app)
+{
+    var scopedFactory = app.Services.GetRequiredService<IServiceScopeFactory>();
+
+    using (var scope = scopedFactory.CreateScope())
+    {
+        var service = scope.ServiceProvider.GetService<SeedDB>();
+        service!.SeedDbAsync().Wait();
+    }
+}
 
 app.UseCors(x => 
     x.AllowAnyMethod()
